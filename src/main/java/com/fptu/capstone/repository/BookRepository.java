0b2c7a7b5837c72478b2cf60710a7fa6d07fe6e7
dthuan <thuanndfw@gmail.com>
2021-06-11 21:54:13 +0700

    add creator home page

--- src/main/java/com/fptu/capstone/repository/BookRepository.java
+++ src/main/java/com/fptu/capstone/repository/BookRepository.java
@@ -8,5 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    List<Book> findAll();
     List<Book> findById(int id);
 }
