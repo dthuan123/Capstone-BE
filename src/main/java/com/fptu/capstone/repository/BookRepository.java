5a033f03b37d56d4d19fe08a9070a10ae8b46c63
vanhub03 <vietanh.dao99@gmail.com>
2021-06-16 03:42:57 +0700

    commit 15/6 listallcategory

--- src/main/java/com/fptu/capstone/repository/BookRepository.java
+++ src/main/java/com/fptu/capstone/repository/BookRepository.java
@@ -1,6 +1,7 @@
 package com.fptu.capstone.repository;
 
 import com.fptu.capstone.entity.Book;
+import com.fptu.capstone.entity.Category;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,5 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book, Integer> {
     List<Book> findAll();
     List<Book> findById(int id);
+    //List<Book> findByCategory(int categoryId);
 }
---
+++ src/main/java/com/fptu/capstone/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.fptu.capstone.repository;
+
+import com.fptu.capstone.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    List<Category> findAll();
+}
