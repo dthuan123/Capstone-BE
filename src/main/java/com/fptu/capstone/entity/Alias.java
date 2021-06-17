70f69bad22c27e07c283ae1bdc36707e361bd021
dthuan <thuanndfw@gmail.com>
2021-06-17 08:55:30 +0700

    alias + new book + audio

---
+++ src/main/java/com/fptu/capstone/entity/Alias.java
@@ -0,0 +1,59 @@
+package com.fptu.capstone.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Alias {
+
+    @Id
+    @Column(name = "alias_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    @JsonIgnore
+    private User user;
+
+    @OneToMany(mappedBy = "alias")
+    @JsonIgnore
+    private List<Book> books;
+
+    @Column(name = "alias_name")
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
