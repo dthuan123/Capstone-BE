05d383bb42391995ec750369cc13b1bfade0e0cc
vanhub03 <vietanh.dao99@gmail.com>
2021-06-24 14:20:07 +0700

    paging and list book top 10

--- src/main/java/com/fptu/capstone/controller/BookController.java
+++ src/main/java/com/fptu/capstone/controller/BookController.java
@@ -12,10 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,6 +33,12 @@ public class BookController {
     @GetMapping("/books")
     public List<Book> getALlBooks() {
         return bookRepository.findAll();
+    }
+
+    @ResponseBody
+    @GetMapping("/book-by-id")
+    public Book getBookById(@RequestHeader int id){
+        return bookRepository.findById(id);
     }
 
     @ResponseBody
