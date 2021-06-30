8462961fea1f25e501663fe1eb0e73a01f9cc038
vanhub03 <vietanh.dao99@gmail.com>
2021-07-01 02:48:35 +0700

    listAllBook

--- src/main/java/com/fptu/capstone/repository/BookRepository.java
+++ src/main/java/com/fptu/capstone/repository/BookRepository.java
@@ -11,11 +11,11 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
-    @Query(value = "select top(10) book_id, status_id, book_name, overall_rating, total_rating, updated_date, description, like_count, enabled,started_date, alias_id, creator_id, image_link from Book", nativeQuery = true)
-    List<Book> findAll();
+    @Query(value = "select top(10) book_id, status_id, book_name, overall_rating, total_rating, updated_date, description, like_count, enabled,started_date, alias_id, creator_id, image_link from Book order by like_count desc", nativeQuery = true)
+    List<Book> findTop10NewestBook();
     Book findById(int id);
     Page<Book> findByCategoriesId(int categoryId, Pageable pageable);
     Page<Book> findALlByCreatorId(int id, Pageable pageable);
     Page<Book> findALlByCreatorIdAndNameContains(int id, String name, Pageable pageable);
-
+    Page<Book> findAll(Pageable pageable);
 }
