4c5d90ba8ff1e878e153c1b5e5c3ed8f7b3e1d98
Tran Viet Anh <anhtvhe130172@fpt.edu.vn>
2021-07-01 02:02:41 +0700

    search and delete message

--- src/main/java/com/fptu/capstone/repository/ReportRepository.java
+++ src/main/java/com/fptu/capstone/repository/ReportRepository.java
@@ -1,13 +1,19 @@
 package com.fptu.capstone.repository;
 
+import com.fptu.capstone.entity.Book;
 import com.fptu.capstone.entity.Report;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
+
 import java.util.List;
 
 public interface ReportRepository extends JpaRepository<Report, Integer> {
     List<Report> findAll();
     List<Report> findAllByUserReceiverId(int id);
     Page<Report> findByUserSenderId(int id, Pageable pageable);
+    Page<Report> findALlByUserSenderIdAndReportContentContains(int id, Pageable pageable, String searchKeyword);
+    ResponseEntity deleteByReportId(int reportId);
 }
