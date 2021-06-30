4cc738d9ff62e13a33af440ab681196e1ee1ded6
Tran Viet Anh <anhtvhe130172@fpt.edu.vn>
2021-07-01 02:02:41 +0700

    search and delete message

--- src/main/java/com/fptu/capstone/controller/ReaderController.java
+++ src/main/java/com/fptu/capstone/controller/ReaderController.java
@@ -10,6 +10,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,4 +31,20 @@ public class ReaderController {
         return reportRepository.findByUserSenderId(userId, pageable);
     }
 
+    @ResponseBody
+    @GetMapping("search-message")
+    public Page<Report> searchMessage(@RequestHeader int page, @RequestHeader int pageSize, @RequestHeader int userId,  @RequestHeader String searchKeyword) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+        if(!searchKeyword.equals("")) {
+            return reportRepository.findALlByUserSenderIdAndReportContentContains(userId, pageable, searchKeyword);
+        }
+        return reportRepository.findByUserSenderId(userId, pageable);
+    }
+
+    @ResponseBody
+    @DeleteMapping(value="delete-message")
+    public ResponseEntity deleteMessage(@RequestHeader int reportId) {
+        reportRepository.deleteByReportId(reportId);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
 }
