de374dabfcdafd4a8428659dd56ba1badcb848bf
thanhnt17 <thanhnthe130328@fpt.edu.vn>
2021-07-22 22:08:51 +0700

    update code

--- src/main/java/com/fptu/capstone/repository/UserRepository.java
+++ src/main/java/com/fptu/capstone/repository/UserRepository.java
@@ -11,10 +11,13 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    @Query("SELECT r FROM User r  WHERE  r.name like %?1%")
+    @Query("SELECT r FROM User r  WHERE  r.name like %?1% AND r.approved = false AND r.role.id = 2")
     Page<User> findAllByName(String name, Pageable pageable);
 
-    @Query("SELECT r FROM User r  WHERE  r.id = ?1")
+    @Query("SELECT r FROM User r  WHERE  r.approved = false AND r.role.id = 2")
+    Page<User> findAll(Pageable pageable);
+
+    @Query("SELECT r FROM User r INNER JOIN Alias  a ON a.user.id = r.id WHERE  a.id = ?1")
     Page<User> findAllById(int id, Pageable pageable);
 
     boolean existsUserByName(String name);
